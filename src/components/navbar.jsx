0f91b45ca6c4132d23c9f778e958d59f8c75c7fe
Franco <frfantusatti@protonmail.com>
2024-12-04 06:15:05 -0300

    add some details

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -22,8 +22,8 @@ export default function Navbar() {
       <ul
         tabIndex={0}
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-        <li><a>Homepage</a></li>
-        <li><a>Portfolio</a></li>
+        <li><a href="index.html">Homepage</a></li>
+        <li><a href='https://github.com/Kenkyoo'>Github</a></li>
         <li><a>About</a></li>
       </ul>
     </div>
